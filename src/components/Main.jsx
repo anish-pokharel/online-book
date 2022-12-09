caaac3eda54722a33ad99962869c8946d4df50b7
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-09 22:21:38 +0545

    axios started

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Main = () => {
-  return <h1>hello form main</h1>;
+  const [search, setSearch] = useState("");
+  return (
+    <>
+      <div className="header">
+        <div className="row1">
+          <h1>
+            A room without a book is like <br />a body without a soul{" "}
+          </h1>
+        </div>
+        <div className="row2">
+          <h2>Find Your Book </h2>
+          <div className="search">
+            <input
+              type="text"
+              placeholder="Enter Your Book Name"
+              value={search}
+            />
+            <button>
+              <i className="fas fa-search"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Main;
