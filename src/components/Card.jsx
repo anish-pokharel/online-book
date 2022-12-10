dbaaacdc9fca17361cb217c77f5cd94002844773
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2022-12-10 15:15:39 +0545

    book

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,44 @@
-import React from "react";
-
-const Card = () => {
+import React, { useState } from "react";
+// import { useState } from "react/cjs/react.development";
+import Modal from "./Modal";
+const Card = ({ book }) => {
+  const [show, setShow] = useState(false);
+  const [bookItem, setItem] = useState();
+  console.log(book);
   return (
     <>
-      <h1>hello from card</h1>
+      {book.map((item, index) => {
+        let thumbnail =
+          item.volumeInfo.imageLinks &&
+          item.volumeInfo.imageLinks.smallThumbnail;
+        let amount = item.saleInfo.listPrice && item.saleInfo.listPrice.amount;
+        if (thumbnail !== undefined && amount !== undefined) {
+          return (
+            <>
+              <div
+                className="card"
+                onClick={() => {
+                  setShow(true);
+                  setItem(item);
+                }}
+                key={index}
+              >
+                <img src={thumbnail} alt="" />
+                <div className="bottom">
+                  <h3 className="title">{item.volumeInfo.title}</h3>
+                  <p className="amount">&#8377;{amount}</p>
+                </div>
+              </div>
+              <Modal
+                show={show}
+                item={bookItem}
+                onClose={() => setShow(false)}
+              />
+            </>
+          );
+        }
+      })}
     </>
   );
 };
-
 export default Card;
